8ad02723291d1320617ac96bca92cacbddac0224
Christian <christian.a.b@live.com>
2026-03-06 11:56:33 -0300

    Primeiro Commit

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,22 +1,94 @@
 /// <reference types="cypress" />
+import { faker } from '@faker-js/faker'
+
+let dadosLogin
+
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-  /*  Como cliente 
-      Quero acessar a Loja EBAC 
-      Para fazer um pedido de 4 produtos 
-      Fazendo a escolha dos produtos
-      Adicionando ao carrinho
-      Preenchendo todas opções no checkout
-      E validando minha compra ao final */
+    /*  Como cliente 
+        Quero acessar a Loja EBAC 
+        Para fazer um pedido de 4 produtos 
+        Fazendo a escolha dos produtos
+        Adicionando ao carrinho
+        Preenchendo todas opções no checkout
+        E validando minha compra ao final */
 
-  beforeEach(() => {
-      cy.visit('/')
-  });
+    before(() => {
+        cy.fixture('perfil').then(perfil => {
+            dadosLogin = perfil
+        })
+    })
 
-  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+
+        // Dados dinâmicos com Faker
+        const nome = faker.person.firstName()
+        const sobrenome = faker.person.lastName()
+        const endereco = faker.location.streetAddress()
+        const cidade = faker.location.city()
+        const email = faker.internet.email()
+
+        const cep = `${faker.string.numeric(5)}-${faker.string.numeric(3)}`
+
+        const telefone = '11987654321'
+
+
+
+        // Login usando comando customizado
+        cy.visit('minha-conta')
+        cy.login(dadosLogin.usuario, dadosLogin.senha)
+
+        cy.get('.page-title').should('contain', 'Minha conta')
+
+        // Ir para produtos
+        cy.visit('produtos')
+        // Adicionar produtos usando comando customizado
+        cy.addProduto(0, 'S', 'Blue')
+        cy.addProduto(1, 'M', 'Yellow')
+        cy.addProduto(2)
+        cy.addProduto(3)
+
+        // Ir para o carrinho
+        cy.get('.dropdown-toggle > .mini-cart-items').click()
+        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .view-cart').click()
+
+        // Checkout
+        cy.contains('Concluir compra').click()
+
+        // Preenchimento do checkout com Faker
+
+
+        cy.get('#billing_first_name').clear({ force: true }).type(nome)
+
+        cy.get('#billing_last_name').clear({ force: true }).type(sobrenome)
+
+        cy.get('#billing_address_1').clear({ force: true }).type(endereco)
+
+        cy.get('#billing_city').clear({ force: true }).type('São Paulo')
+
+        cy.get('#billing_postcode').clear({ force: true }).type('01012-000')
+
+        cy.get('#billing_phone').clear({ force: true }).type('11987654321')
+
+        cy.get('#billing_email').clear({ force: true }).type(email)
+
+        cy.get('#terms').click()
+
+        cy.get('#place_order').should('be.visible').click()
+
+        cy.contains('Obrigado. Seu pedido foi recebido').should('be.visible')
+
+
+
+        // espera a atualização do checkout
+        cy.wait(2000)
+
       
-  });
-
-
-})
+        // validação final
+        cy.contains('Obrigado. Seu pedido foi recebido').should('be.visible')
+    });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,3 +4,20 @@ Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('.woocommerce-form > .button').click()
 });
 
+Cypress.Commands.add('addProduto', (index, tamanho, cor) => {
+  cy.visit('produtos')
+
+  cy.get('.product-block')
+    .eq(index)
+    .click()
+
+  if (tamanho) {
+    cy.get(`.button-variable-item-${tamanho}`).click()
+  }
+
+  if (cor) {
+    cy.get(`.button-variable-item-${cor}`).click()
+  }
+
+  cy.get('.single_add_to_cart_button').click()
+})
